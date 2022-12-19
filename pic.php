b87c04b7815de156bb94eba5fb3fa9c52d6e261f
darren-arch <118233568+darren-arch@users.noreply.github.com>
2022-12-18 23:30:17 -0600

    Add files via upload

---
+++ pic.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Monty's Website</title>
+  <style>
+    body {
+      text-align: center;
+      font-family: sans-serif;
+    }
+    img {
+      width: 25%;
+      height: 25%;
+    }
+  </style>
+</head>
+<body>
+  <h1>Meet Monty</h1>
+  <img src="https://static01.nyt.com/images/2019/06/17/science/17DOGS/17DOGS-mobileMasterAt3x-v2.jpg" alt="A picture of Monty the dog">
+  <p>This is Monty</p>
+</body>
+</html>
